6c1285cbf9dba9607d1594ab0f99198d355796a0
Anurag Sood <anurag.sood007@gmail.com>
2019-03-14 16:37:40 +0530

    Changes

--- src/main/java/com/anurag/schoolrecord/signup/SignupController.java
+++ src/main/java/com/anurag/schoolrecord/signup/SignupController.java
@@ -1,5 +1,28 @@
 package com.anurag.schoolrecord.signup;
 
-public class SignupController {
+import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.anurag.schoolrecord.database_entities.User;
+
+@RestController
+public class SignupController {
+	
+	@Autowired
+	SignupServices signUpServices;
+
+	@RequestMapping("/users")
+	public Iterable<User> getAllUsers(){
+		return signUpServices.getAllUser();
+	}
+	
+	@RequestMapping(method=RequestMethod.POST,value="/signup")
+	public void signup(@RequestBody User user) {
+		signUpServices.addUser(user);;
+	}
 }
--- src/main/java/com/anurag/schoolrecord/signup/SignupServices.java
+++ src/main/java/com/anurag/schoolrecord/signup/SignupServices.java
@@ -1,6 +1,26 @@
 package com.anurag.schoolrecord.signup;
 
-public class SignupServices {
+import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.anurag.schoolrecord.database_entities.User;
+import com.anurag.schoolrecord.database_entities.UserRepository;
+
+@Service
+public class SignupServices {
+	@Autowired
+	private UserRepository userRepository;
 	
+	public ArrayList<User> users = new ArrayList<>();
+	
+	public void addUser(User user) {
+		users.add(user);
+		userRepository.save(user);
+	}
+	
+	public Iterable<User> getAllUser(){
+		return userRepository.findAll();
+	}
 }
