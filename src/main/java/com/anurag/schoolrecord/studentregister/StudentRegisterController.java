353ca9fe63509812129521c8da571920e995d7b2
Anurag Sood <anurag.sood007@gmail.com>
2019-03-15 18:28:58 +0530

    Added addStudent() and getStudent() for adding and getting the studnt details

--- src/main/java/com/anurag/schoolrecord/studentregister/StudentRegisterController.java
+++ src/main/java/com/anurag/schoolrecord/studentregister/StudentRegisterController.java
@@ -1,5 +1,36 @@
 package com.anurag.schoolrecord.studentregister;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.anurag.schoolrecord.database_entities.Student;
+
+@RestController
 public class StudentRegisterController {
 	
+	@Autowired
+	private StudentRegisterServices studentRegisterServices;
+	
+	//add a student
+	@RequestMapping(value = "/student", method = RequestMethod.POST)
+	public void addStudent(Student student) {
+		studentRegisterServices.addStudent(student);
+	}
+	//get a student by its rollNo
+	@RequestMapping("/student?roll_no=")
+	public List<Student> getStudentByRollNo(Iterable<Integer> rollNo){
+		return studentRegisterServices.findStudentByRollNo(rollNo);
+	}
+	
+	//get all students details
+	@RequestMapping("/student")
+	public ArrayList<Student> getAllStudents() {
+		return studentRegisterServices.getAllStudent();
+	}
 }
--- src/main/java/com/anurag/schoolrecord/studentregister/StudentRegisterServices.java
+++ src/main/java/com/anurag/schoolrecord/studentregister/StudentRegisterServices.java
@@ -1,11 +1,36 @@
 package com.anurag.schoolrecord.studentregister;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.anurag.schoolrecord.database_entities.Student;
 
 @Service
 public class StudentRegisterServices {
 	
 	@Autowired
 	private StudentRepository studentRepository;
+	
+	//Add Student details
+	public void addStudent(Student student) {
+		studentRepository.save(student);
+	}
+	
+	//Get a student by its rollNo
+	public List<Student> findStudentByRollNo(Iterable<Integer> rollNo){
+		return studentRepository.findAllById(rollNo);
+	}
+	
+	//Get all student details
+	public ArrayList<Student> getAllStudent(){
+		ArrayList<Student> students = new ArrayList<>();
+		studentRepository.findAll().forEach(students::add);
+		return students;
+	}
+	//Delete a student details
+	//update a student record
 }
